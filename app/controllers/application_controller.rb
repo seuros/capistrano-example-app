455d8a6929a6fa5607b97e8bdfa9c6c9409c84a9
Abdelkader Boudih <terminale@gmail.com>
2022-10-08 17:58:47 +0100

    Output user running puma

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def index
-    render plain: 'Hello, world!'
+    render plain: "Hello, world! Puma running on #{Socket.gethostname} with PID #{Process.pid} and Ruby #{RUBY_VERSION} as #{ENV['USER']}"
   end
 end
